8e0e43a4fca8db7fc846e98c6cf97dd14cc55e64
John Pham <mr.john.pham@gmail.com>
2020-04-18 06:06:17 -0400

    Hello world

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
 'use strict';
 
-var React = require('react');
-var ReactDOM = require('react-dom');
-var App = require('./components/App');
-
-ReactDOM.render(
-    <App />,
-    document.getElementById('app')
-);
+let slide = document.getElementById("app");
+slide.innerHTML = "Hello world";

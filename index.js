8a1be51717dc4691bab98dc1313447a42bdcacfe
John Pham <mr.john.pham@gmail.com>
2020-04-18 06:24:08 -0400

    Create advanceSlide helper function

--- index.js
+++ index.js
@@ -11,5 +11,14 @@ let picturesArray = [
 let currentSlide = 0;
 let maxSlides = picturesArray.length;
 
+const advanceSlide = () => {
+    //return the increment of currentSlide unless it is at the last slide. If so, return zero;
+    return ++currentSlide == maxSlides ?
+        0 :
+        slideNumber;
+}
+
 let slide = document.getElementById("app");
+slide.innerHTML = `<img src="${picturesURL + picturesArray[currentSlide]}" alt="Slideshow">`;
+currentSlide = advanceSlide(); 
 slide.innerHTML = `<img src="${picturesURL + picturesArray[currentSlide]}" alt="Slideshow">`;

baa4adc7ac169c180215d14256d6fd1790ab5eea
John Pham <mr.john.pham@gmail.com>
2020-04-18 06:12:23 -0400

    Display initial picture

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 'use strict';
 
+let picturesURL = "./feature/";
+
+let picturesArray = [
+    "feature_back_of_head.jpg"
+];
+
 let slide = document.getElementById("app");
-slide.innerHTML = "Hello world";
+//slide.innerHTML = "Hello world";
+slide.innerHTML = `<img src="${picturesURL + picturesArray[0]}" alt="Slideshow">`;

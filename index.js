f648b80901cf444fd1adb33dfb32e9af13834954
John Pham <mr.john.pham@gmail.com>
2020-04-19 18:41:58 -0400

    Add comment headers to code

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 'use strict';
 
-
-
 let test = document.getElementById("app");
 const URL = "http://www.john-pham.com/audreybergensten/feature/";
 let picturesArray = [
@@ -11,8 +9,9 @@ let picturesArray = [
         ];
 
 let count = 0;
-let max = 3;
+let max = picturesArray.length;
 
+/*** Pre-fetch images */
 window.onload = () => {
     setTimeout(() => {
         for (let i = 0; i < picturesArray.length; i++) {
@@ -22,7 +21,7 @@ window.onload = () => {
     }, 1000)
 };
 
-
+/*** Display next slide */
 const display = () => {
 
   if (count === max) {
@@ -34,47 +33,5 @@ const display = () => {
   
 }
 
-let timer = setInterval(display, 1000);
-
-
-/*
-const advanceSlide = (slideNumber, maxSlides) => {
-    //return the increment of currentSlide unless it is at the last slide. If so, return zero;
-    return slideNumber + 1 === maxSlides ?
-        0 :
-        slideNumber + 1;
-}
-
-const reducer = () => {};
-
-class Slideshow {
-    constructor() {
-        this.currentSlide = -1; //calling displaySlide will automatically increment this to 0
-        this.picturesURL = "./feature/";
-        this.picturesArray = [
-            "feature_back_of_head.jpg",
-            "feature_blastula.jpg",
-            "feature_blue_figure.jpg"
-        ];
-    }
-
-    displaySlide() {
-        this.currentSlide = advanceSlide(this.currentSlide, this.picturesArray.length); //increment
-        //return `<img src="${this.picturesURL + this.picturesArray[this.currentSlide]}" alt="Slideshow">`; //return the img tag
-        document.getElementById("app").innerHTML = `<img src="${this.picturesURL + this.picturesArray[this.currentSlide]}" alt="Slideshow">`;
-    }
-}
-
-let mySlideshow = new Slideshow();
-//document.getElementById("app").innerHTML = mySlideshow.displaySlide();
-mySlideshow.displaySlide();
-
-
-//let slide = document.getElementById("app");
-//slide.innerHTML = `<img src="${picturesURL + picturesArray[currentSlide]}" alt="Slideshow">`;
-//currentSlide = advanceSlide(currentSlide); 
-//slide.innerHTML  = currentSlide;
-//slide.innerHTML = `<img src="${picturesURL + picturesArray[currentSlide]}" alt="Slideshow">`;
-
-let timer = setInterval(mySlideshow.displaySlide(), 100); //need to debug
-*/
+/*** Advance to the next slide every one second */
+let slideShowTimer = setInterval(display, 1000);

e618a05f215786dc5eeb20e46987ff7c4d47a105
John Pham <mr.john.pham@gmail.com>
2020-04-19 18:46:06 -0400

    Add more pictures to array

--- index.js
+++ index.js
@@ -5,7 +5,23 @@ const URL = "http://www.john-pham.com/audreybergensten/feature/";
 let picturesArray = [
             "feature_back_of_head.jpg",
             "feature_blastula.jpg",
-            "feature_blue_figure.jpg"
+            "feature_blue_figure.jpg",
+            "feature_cosmic_waves.jpg",
+            "feature_crossed_figures.jpg",
+            "feature_dream.jpg",
+            "feature_eye_beam.jpg",
+            "feature_eyesmouths.jpg",
+            "feature_flyer.jpg",
+            "feature_head_on_hill.jpg",
+            "feature_heads_gears.jpg",
+            "feature_klees_trees.jpg",
+            "feature_nudie.jpg",
+            "feature_pink_cheeks.jpg",
+            "feature_pink_snowman.jpg",
+            "feature_red_box.jpg",
+            "feature_silver-gold.jpg",
+            "feature_wavy_hair.jpg",
+            "feature_wrapped_figures.jpg"
         ];
 
 let count = 0;
